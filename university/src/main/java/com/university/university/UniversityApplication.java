dc32c0f99462c2a4f713106a3eae50eb8ddecc7a
aman <amanmagrawal2002@gmail.com>
2024-02-03 14:43:25 +0530

    Added Student CRUD operations

--- university/src/main/java/com/university/university/UniversityApplication.java
+++ university/src/main/java/com/university/university/UniversityApplication.java
@@ -14,25 +14,7 @@ public class UniversityApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = SpringApplication.run(UniversityApplication.class, args);
-		UserRepository userRepository = context.getBean(UserRepository.class);
-
-		userRepository.deleteAll();
-		User user1 = new User();
-		user1.setName("Aman");
-		user1.setCity("Pune");
-		user1.setCourse("Computer");
-
-		User user2 = new User();
-		user2.setName("Aryan");
-		user2.setCity("Nagpur");
-		user2.setCourse("Mechanical");
-
-		List<User> users = List.of(user1,user2);
-		Iterable<User> result = userRepository.saveAll(users);
-		result.forEach(user ->{
-			System.out.println(user.getId()+" "+user.getName()+" "+user.getCity()+" "+user.getCourse());
-		});
+		SpringApplication.run(UniversityApplication.class, args);
 
 	}
 
---
+++ university/src/main/java/com/university/university/controllers/StudentController.java
@@ -0,0 +1,65 @@
+package com.university.university.controllers;
+
+import com.university.university.entities.Student;
+import com.university.university.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getStudents(){
+        List<Student> list = this.studentService.allStudents();
+        if(list.size()<=0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(list));
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable ("id") int id){
+        Optional<Student> result = this.studentService.findStudentById(id);
+        if(result.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.of(result);
+    }
+
+    @PostMapping("/register")
+    public Student createStudent(@RequestBody Student student){
+        return this.studentService.addStudent(student);
+    }
+
+
+    @DeleteMapping("/students/{Id}")
+    public ResponseEntity<List<Student>> deleteStudentData(@PathVariable ("Id") int Id){
+
+        List<Student> result = this.studentService.allStudents();
+        List<Student> result1 = this.studentService.deleteStudentById(Id);
+        if(result.size()==result1.size()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.of(Optional.of(result1));
+
+    }
+
+
+    @PutMapping("/students/{Id}")
+    public Student updateStudentData(@RequestBody Student student ,@PathVariable ("Id") int Id){
+        student = this.studentService.updateStudent(student, Id);
+        return student;
+    }
+
+
+
+
+}
